98886b737eb3eb97ce3be03b18ddf8f11c039717
Paceto <paceto@paceto.net>
2022-05-11 11:32:10 +0300

    add alerts

--- src/fetch-alerts.ts
+++ src/fetch-alerts.ts
@@ -19,6 +19,7 @@ export const fetchAlerts = async (
             node {
               id
               repository {
+                vulnerabilityAlerts
                 name
                 owner {
                   login
